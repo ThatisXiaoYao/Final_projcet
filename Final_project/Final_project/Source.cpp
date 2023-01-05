388d971392c05923693a7776dcda74a60370ad61
Liang22tw <t111360215@ntut.org.tw>
2023-01-05 22:22:06 +0800

    update

--- Final_project/Final_project/Source.cpp
+++ Final_project/Final_project/Source.cpp
@@ -40,7 +40,7 @@ int main()
 {
 	srand(time(0));
 
-	RenderWindow window(VideoMode(720, 720), "The Game!");
+	RenderWindow window(VideoMode(700, 740), "The Game!");
 
 	Texture t1, t2, t3, t4, t5, t6, t7;
 	t1.loadFromFile("./tiles_DIY.png");
@@ -69,10 +69,11 @@ int main()
 	bool move_ = false;
 	bool Latch = false;
 	bool score_chage= false;
+	bool Lose = false;
 
 	Clock clock;
 
-	printf("Level %d\n", Level);
+	Event e;
 
 	block_now = rand() % 7;
 	colorNum = 1 + block_now;
@@ -100,57 +101,98 @@ int main()
 		clock.restart();
 		timer += time;
 
-		Event e;
-		while (window.pollEvent(e))
+		if (Lose)
 		{
-			if (e.type == Event::Closed)
-				window.close();
+			while (window.pollEvent(e))
+			{
+				if (e.type == Event::Closed)
+					window.close();
+			}
 
-			if (e.type == Event::KeyPressed)
-				if (e.key.code == Keyboard::K || e.key.code == Keyboard::W) rotate = 1;
-				else if (e.key.code == Keyboard::J) rotate = 2;
-				else if (e.key.code == Keyboard::A) dx = -1;
-				else if (e.key.code == Keyboard::D) dx = 1;
-				else if (e.key.code == Keyboard::Space) drop = true;
-				else if (e.key.code == Keyboard::X) Hold = 1;
+			for (int i = 0; i < width; i++)
+			{
+				for (int j = 0; j < high; j++)
+				{
+					cell.setPosition(10 + i * 36, 10 + j * 36);
+					window.draw(cell);
+				}
+			}
+
+			box.setPosition(410, 40);
+			window.draw(box);
+
+			Flag.setTextureRect(IntRect(Hold * 15, 0, 15, 15));
+			Flag.setPosition(420, 50);
+			window.draw(Flag);
+
+			Score_bord.setPosition(385, 580);
+			window.draw(Score_bord);
+
+			Number.setTextureRect(IntRect(Level * 15, 0, 15, 25));
+			Number.setPosition(490, 605);
+			window.draw(Number);
+
+			for (int i = 0; i < 10; i++)
+			{
+				Number.setTextureRect(IntRect(score_array[i] * 15, 0, 15, 25));
+				Number.setPosition(490 + i * 15, 665);
+				window.draw(Number);
+			}
+
+			for (int i = 0; i < high; i++)
+			{
+				for (int j = 0; j < width; j++)
+				{
+					if (field[i][j] == 0) continue;
+					s_.setPosition(10 + j * 36, 10 + i * 36);
+					window.draw(s_);
+				}
+			}
 		}
-
-		if (block_now == 6) rotate = 0;
-
-		if (Keyboard::isKeyPressed(Keyboard::S)) delay = 0.05;
-		else delay = 1.0 - 0.1 * Level;
-
-		//// <- Move -> ///
-		for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].x += dx; move_ = 1;}
-		dx = 0;							//
-
-		for (int i = 0; i < 4; i++) { shadow[i].x = a[i].x; shadow[i].y = a[i].y; }
-
-		if (!check(a)) for (int i = 0; i < 4; i++) a[i] = b[i];
-		if (drop)delay = 0;				//
-
-		//////Rotate//////
-		if (rotate == 1)
+		else
 		{
-			Point p = a[2]; //center of rotation
-			for (int i = 0; i < 4; i++)
+			while (window.pollEvent(e))
 			{
-				int x = a[i].y - p.y;
-				int y = a[i].x - p.x;
-				a[i].x = p.x - x;
-				a[i].y = p.y + y;
+				if (e.type == Event::Closed)
+					window.close();
+
+				if (e.type == Event::KeyPressed)
+				{
+					if (e.key.code == Keyboard::K || e.key.code == Keyboard::W) rotate = 1;
+					else if (e.key.code == Keyboard::J) rotate = 2;
+					else if (e.key.code == Keyboard::A) dx = -1;
+					else if (e.key.code == Keyboard::D) dx = 1;
+					else if (e.key.code == Keyboard::Space) drop = true;
+					else if (e.key.code == Keyboard::X) Hold = 1;
+				}
 			}
 
-			for (int i = 0; i < 4; i++)
-			{
-				if (a[i].x < 0) for (int j = 0; j < 4; j++) a[j].x++;
-				else if (a[i].x >= width) for (int j = 0; j < 4; j++) a[j].x--;
-				else if (a[i].y < 0) for (int j = 0; j < 4; j++) a[j].y++;
-				else if (field[a[i].y][a[i].x]) for (int j = 0; j < 4; j++) a[j] = b[j];
-			}
+			if (block_now == 6) rotate = 0;
 
-			if (block_now == 0)
+			if (Keyboard::isKeyPressed(Keyboard::S)) delay = 0.05;
+			else delay = 1.0 - 0.1 * Level;
+
+			//// <- Move -> ///
+			for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].x += dx; move_ = 1; }
+			dx = 0;							//
+
+			for (int i = 0; i < 4; i++) { shadow[i].x = a[i].x; shadow[i].y = a[i].y; }
+
+			if (!check(a)) for (int i = 0; i < 4; i++) a[i] = b[i];
+			if (drop)delay = 0;				//
+
+			//////Rotate//////
+			if (rotate == 1)
 			{
+				Point p = a[2]; //center of rotation
+				for (int i = 0; i < 4; i++)
+				{
+					int x = a[i].y - p.y;
+					int y = a[i].x - p.x;
+					a[i].x = p.x - x;
+					a[i].y = p.y + y;
+				}
+
 				for (int i = 0; i < 4; i++)
 				{
 					if (a[i].x < 0) for (int j = 0; j < 4; j++) a[j].x++;
@@ -158,284 +200,287 @@ int main()
 					else if (a[i].y < 0) for (int j = 0; j < 4; j++) a[j].y++;
 					else if (field[a[i].y][a[i].x]) for (int j = 0; j < 4; j++) a[j] = b[j];
 				}
+
+				if (block_now == 0)
+				{
+					for (int i = 0; i < 4; i++)
+					{
+						if (a[i].x < 0) for (int j = 0; j < 4; j++) a[j].x++;
+						else if (a[i].x >= width) for (int j = 0; j < 4; j++) a[j].x--;
+						else if (a[i].y < 0) for (int j = 0; j < 4; j++) a[j].y++;
+						else if (field[a[i].y][a[i].x]) for (int j = 0; j < 4; j++) a[j] = b[j];
+					}
+				}
+
+				for (int i = 0; i < 4; i++) { shadow[i].x = a[i].x; shadow[i].y = a[i].y; }
+
+				move_ = true;
+				rotate = 0;
 			}
 
-			for(int i = 0; i < 4; i++) { shadow[i].x = a[i].x; shadow[i].y = a[i].y; }
-
-			move_ = true;
-			rotate = 0;
-		}
-
-		if (rotate == 2)			//
-		{
-			Point p = a[2]; //center of rotation
-			for (int i = 0; i < 4; i++)
+			if (rotate == 2)			//
 			{
-				int x = a[i].y - p.y;
-				int y = a[i].x - p.x;
-				a[i].x = p.x + x;
-				a[i].y = p.y - y;
-			}
-
-			for (int i = 0; i < 4; i++)
-			{
-				if (a[i].x < 0) for (int j = 0; j < 4; j++) a[j].x++;
-				else if (a[i].x >= width) for (int j = 0; j < 4; j++) a[j].x--;
-				else if (a[i].y < 0) for (int j = 0; j < 4; j++) a[j].y++;
-				else if (field[a[i].y][a[i].x]) for (int j = 0; j < 4; j++) a[j] = b[j];
-			}
-
-			for (int i = 0; i < 4; i++) { shadow[i].x = a[i].x; shadow[i].y = a[i].y; }
-			
-			move_ = true;
-			rotate = 0;
-		}
-
-		////Hold////
-		if (Hold == 1 && !Latch)
-		{
-			for (int i = 0; i < 4; i++)
-			{
-				a[i].x = (figures[block_now][i] / 2);
-				a[i].y = figures[block_now][i] % 2;
-			}
-
-			int hold;
-			hold= block_now;
-			block_now = block_next;
-			block_next = hold;
-
-			colorNum = 1 + block_now;
-			colorNum_next = 1 + block_next;
-
-			Point hold_p[4];
-			for (int i = 0; i < 4; i++)
-			{
-				hold_p[i] = a[i];
-				a[i] = next[i];
-				next[i] = hold_p[i];
-
-				a[i].x += 3;
-
-				shadow[i] = a[i];
-			}
-			Latch = true;
-		}
-
-		///////Tick//////
-		if (timer > delay)
-		{
-			for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].y += 1; }
-
-			if (!check(a))
-			{
-				for (int i = 0; i < 4; i++) field[b[i].y][b[i].x] = colorNum;
-
-				block_now = block_next;
-				colorNum = 1 + block_now;
+				Point p = a[2]; //center of rotation
+				for (int i = 0; i < 4; i++)
+				{
+					int x = a[i].y - p.y;
+					int y = a[i].x - p.x;
+					a[i].x = p.x + x;
+					a[i].y = p.y - y;
+				}
 
 				for (int i = 0; i < 4; i++)
 				{
-					a[i].x = 3 + (figures[block_now][i] / 2);
-					a[i].y = figures[block_now][i] % 2;
-
-					shadow[i].x = 3 + (figures[block_now][i] / 2);
-					shadow[i].y = figures[block_now][i] % 2;
+					if (a[i].x < 0) for (int j = 0; j < 4; j++) a[j].x++;
+					else if (a[i].x >= width) for (int j = 0; j < 4; j++) a[j].x--;
+					else if (a[i].y < 0) for (int j = 0; j < 4; j++) a[j].y++;
+					else if (field[a[i].y][a[i].x]) for (int j = 0; j < 4; j++) a[j] = b[j];
 				}
 
-				block_next = rand() % 7;
+				for (int i = 0; i < 4; i++) { shadow[i].x = a[i].x; shadow[i].y = a[i].y; }
+
+				move_ = true;
+				rotate = 0;
+			}
+
+			////Hold////
+			if (Hold == 1 && !Latch)
+			{
+				for (int i = 0; i < 4; i++)
+				{
+					a[i].x = (figures[block_now][i] / 2);
+					a[i].y = figures[block_now][i] % 2;
+				}
+
+				int hold;
+				hold = block_now;
+				block_now = block_next;
+				block_next = hold;
+
+				colorNum = 1 + block_now;
 				colorNum_next = 1 + block_next;
 
+				Point hold_p[4];
 				for (int i = 0; i < 4; i++)
 				{
-					next[i].x = figures[block_next][i] / 2;
-					next[i].y = figures[block_next][i] % 2;
+					hold_p[i] = a[i];
+					a[i] = next[i];
+					next[i] = hold_p[i];
+
+					a[i].x += 3;
+
+					shadow[i] = a[i];
 				}
-
-				Hold = 0;
-				Latch = false;
-				drop = false;			//
-				delay = 1.0 - 0.1 * Level;
+				Latch = true;
 			}
-			timer = 0;
-		}
 
-		////shadow/////
-		if (move_)
-		{
-			for (int i = 0; i < 4; i++) { shadow[i].x = a[i].x; shadow[i].y = a[i].y; }
-
-			for (int i = 0; i < 20; i++)
+			///////Tick//////
+			if (timer > delay)
 			{
-				for (int j = 0; j < 4; j++) b[j]= shadow[j];
+				for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].y += 1; }
 
-				for (int j = 0; j < 4; j++) shadow[j].y ++;
-
-				if (!check(shadow))
+				if (!check(a))
 				{
-					for (int j = 0; j < 4; j++) shadow[j] = b[j];
+					for (int i = 0; i < 4; i++) field[b[i].y][b[i].x] = colorNum;
+
+					block_now = block_next;
+					colorNum = 1 + block_now;
+
+					for (int i = 0; i < 4; i++)
+					{
+						a[i].x = 3 + (figures[block_now][i] / 2);
+						a[i].y = figures[block_now][i] % 2;
+
+						shadow[i].x = 3 + (figures[block_now][i] / 2);
+						shadow[i].y = figures[block_now][i] % 2;
+					}
+
+					block_next = rand() % 7;
+					colorNum_next = 1 + block_next;
+
+					for (int i = 0; i < 4; i++)
+					{
+						next[i].x = figures[block_next][i] / 2;
+						next[i].y = figures[block_next][i] % 2;
+					}
+
+					////Lose////
+					for (int i = 0; i < 4; i++) if (field[a[i].y][a[i].x] != 0)	Lose = true;
+
+					if (Lose) for (int i = 0; i < 4; i++) field[a[i].y][a[i].x] = colorNum;
+
+					Hold = 0;
+					Latch = false;
+					drop = false;			//
+					delay = 1.0 - 0.1 * Level;
+				}
+				timer = 0;
+			}
+
+			////shadow/////
+			if (move_)
+			{
+				for (int i = 0; i < 4; i++) { shadow[i].x = a[i].x; shadow[i].y = a[i].y; }
+
+				for (int i = 0; i < 20; i++)
+				{
+					for (int j = 0; j < 4; j++) b[j] = shadow[j];
+
+					for (int j = 0; j < 4; j++) shadow[j].y++;
+
+					if (!check(shadow))
+					{
+						for (int j = 0; j < 4; j++) shadow[j] = b[j];
+						break;
+					}
+				}
+				move_ = false;
+			}
+
+			///////check lines//////////
+			int k = high - 1;
+			for (int i = high - 1; i > 0; i--)
+			{
+				int count = 0;
+				for (int j = 0; j < width; j++)
+				{
+					if (field[i][j]) count++;
+					field[k][j] = field[i][j];
+				}
+				if (count < width) k--;
+				else if (count == width)
+					remove_counter++;
+			}
+
+
+			////Level  &  score////
+			if (remove_counter != 0)
+			{
+				////score////
+				switch (remove_counter)
+				{
+
+				case 1:
+					score = score + 40 * (Level + 1);
+					break;
+				case 2:
+					score = score + 100 * (Level + 1);
+					break;
+				case 3:
+					score = score + 300 * (Level + 1);
+					break;
+				case 4:
+					score = score + 1200 * (Level + 1);
 					break;
 				}
-			}
-			move_ = false;
-		}
 
-		///////check lines//////////
-		int k = high - 1;
-		for (int i = high - 1; i > 0; i--)
-		{
-			int count = 0;
-			for (int j = 0; j < width; j++)
-			{
-				if (field[i][j]) count++;
-				field[k][j] = field[i][j];
-			}
-			if (count < width) k--;
-			else if (count == width)
-				remove_counter++;
-		}
+				unsigned long long score_ = score;
 
-
-		////Level  &  score////
-		if (remove_counter != 0)
-		{
-			////score////
-			switch (remove_counter)
-			{
-
-			case 1:
-				printf("You fisished a single !\n");
-				score = score + 40 * (Level + 1);
-				break;
-			case 2:
-				printf("You fisished a double !!\n");
-				score = score + 100 * (Level + 1);
-				break;
-			case 3:
-				printf("You fisished a triple !!!\n");
-				score = score + 300 * (Level + 1);
-				break;
-			case 4:
-				printf("You fisished a Tetris !!!!\n");
-				score = score + 1200 * (Level + 1);
-				break;
-			}
-
-			unsigned long long score_ = score;
-
-			for (int i = 9; i > 0; i--)
-			{
-				score_array[i] = score_%10;
-				score_ = score_ / 10;
-			}
-
-			score_chage = true;
-
-			printf("now score: %d\n", score);
-			
-			for (int i = 0; i < remove_counter; i++)
-			{
-				Level_counter ++;
-				if (Level_counter % 10 == 0 && Level < 9)
+				for (int i = 9; i > 0; i--)
 				{
-					Level++;
-					printf("\nLevel %d\n\n", Level);
+					score_array[i] = score_ % 10;
+					score_ = score_ / 10;
+				}
+
+				score_chage = true;
+
+				for (int i = 0; i < remove_counter; i++)
+				{
+					Level_counter++;
+					if (Level_counter % 10 == 0 && Level < 9) Level++;
+				}
+				remove_counter = 0;
+			}
+
+
+
+			/////////draw//////////
+			window.clear(Color::Black);
+
+
+			////background////
+			for (int i = 0; i < width; i++)
+			{
+				for (int j = 0; j < high; j++)
+				{
+					cell.setPosition(10 + i * 36, 10 + j * 36);
+					window.draw(cell);
 				}
 			}
-			remove_counter = 0;
-		}
 
+			box.setPosition(410, 40);
+			window.draw(box);
 
+			Flag.setTextureRect(IntRect(Hold * 15, 0, 15, 15));
+			Flag.setPosition(420, 50);
+			window.draw(Flag);
 
-		/////////draw//////////
-		window.clear(Color::Black);
+			Score_bord.setPosition(385, 580);
+			window.draw(Score_bord);
 
-
-		////background////
-		unsigned char CELL_SIZE = 36;
-		unsigned char COLUMNS = 10;
-		unsigned char ROWS = 20;
-		unsigned char SCREEN_RESIZE = 4;
-
-		for (unsigned char i = 0; i < COLUMNS; i++)
-		{
-			for (unsigned char j = 0; j < ROWS; j++)
-			{
-				cell.setPosition(CELL_SIZE*i, CELL_SIZE*j);
-				window.draw(cell);
-			}
-		}
-
-		box.setPosition(400, 30);
-		window.draw(box);
-
-		Flag.setTextureRect(IntRect(Hold * 15, 0, 15, 15));
-		Flag.setPosition(410, 40);
-		window.draw(Flag);
-
-		Score_bord.setPosition(375, 570);
-		window.draw(Score_bord);
-
-		Number.setTextureRect(IntRect(Level * 15, 0, 15, 25));
-		Number.setPosition(480, 595);
-		window.draw(Number);
-
-		for (int i = 0; i < 10; i++)
-		{
-			Number.setTextureRect(IntRect(score_array[i] * 15, 0, 15, 25));
-			Number.setPosition(480 + i * 15, 655);
+			Number.setTextureRect(IntRect(Level * 15, 0, 15, 25));
+			Number.setPosition(490, 605);
 			window.draw(Number);
-		}
 
-		////blocks////
-		for (int i = 0; i < high; i++)
-		{
-			for (int j = 0; j < width; j++)
+			for (int i = 0; i < 10; i++)
 			{
-				if (field[i][j] == 0) continue;
-				s.setTextureRect(IntRect(field[i][j] * 36, 0, 36, 36));
-				s.setPosition(j * 36, i * 36);
+				Number.setTextureRect(IntRect(score_array[i] * 15, 0, 15, 25));
+				Number.setPosition(490 + i * 15, 665);
+				window.draw(Number);
+			}
+
+			////blocks////
+
+			for (int i = 0; i < high; i++)
+			{
+				for (int j = 0; j < width; j++)
+				{
+					if (field[i][j] == 0) continue;
+					s.setTextureRect(IntRect(field[i][j] * 36, 0, 36, 36));
+					s.setPosition(10 + j * 36, 10 + i * 36);
+					window.draw(s);
+				}
+			}
+
+			for (int i = 0; i < 4; i++)
+			{
+				s_.setPosition(10 + shadow[i].x * 36, 10 + shadow[i].y * 36);
+				window.draw(s_);
+			}
+
+			for (int i = 0; i < 4; i++)
+			{
+				s.setTextureRect(IntRect(colorNum * 36, 0, 36, 36));
+				s.setPosition(10 + a[i].x * 36, 10 + a[i].y * 36);
+				window.draw(s);
+			}
+
+			for (int i = 0; i < 4; i++)
+			{
+				s.setTextureRect(IntRect(colorNum_next * 36, 0, 36, 36));
+				s.setPosition(10 + next[i].x * 36, 10 + next[i].y * 36);
+
+				switch (block_next)
+				{
+				case 0:
+					s.move(448, 130);
+					break;
+				case 1:
+				case 2:
+				case 3:
+				case 4:
+				case 5:
+					s.move(430, 130);
+					break;
+				case 6:
+					s.move(448, 130);
+					break;
+				}
 				window.draw(s);
 			}
 		}
 
-		for (int i = 0; i < 4; i++)
-		{
-			s_.setPosition(shadow[i].x * 36, shadow[i].y * 36);
-			window.draw(s_);
-		}
 
-		for (int i = 0; i < 4; i++)
-		{
-			s.setTextureRect(IntRect(colorNum * 36, 0, 36, 36));
-			s.setPosition(a[i].x * 36, a[i].y * 36);
-			window.draw(s);
-		}
-
-		for (int i = 0; i < 4; i++)
-		{
-			s.setTextureRect(IntRect(colorNum_next * 36, 0, 36, 36));
-			s.setPosition(next[i].x * 36, next[i].y * 36);
-
-			switch (block_next)
-			{
-			case 0:
-				s.move(438, 120);
-				break;
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-			case 5:
-				s.move(420, 120);
-				break;
-			case 6:
-				s.move(438, 120);
-				break;
-			}
-
-			window.draw(s);
-		}
 
 		window.display();
 	}
